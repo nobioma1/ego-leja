9d77f170734d90444f6ab565cca4110f15cbfcea
Noble Obioma <nobioma1@gmail.com>
2020-05-04 00:33:27 +0100

    Add AppRoute to App Compnent

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,12 +1,16 @@
-import { CSSReset } from '@chakra-ui/core';
+import { CSSReset, Box } from '@chakra-ui/core';
 import React from 'react';
 
 import { ThemeProvider } from 'components/Theme';
+import { AppRoute } from 'components/Routes';
 
 export const App = () => {
   return (
     <ThemeProvider>
       <CSSReset />
+      <Box>
+        <AppRoute />
+      </Box>
     </ThemeProvider>
   );
 };
