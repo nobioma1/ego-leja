08d9a54875c85cb5a4bcba8456496379a1844cb9
Noble Obioma <nobioma1@gmail.com>
2020-05-04 00:32:34 +0100

    Create DrawerLayout and Modal UIComponent

---
+++ client/src/components/Modal/Modal.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  SlideIn,
+  Modal as UIModal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  Divider,
+  ModalCloseButton,
+} from '@chakra-ui/core';
+
+export const Modal = ({ isOpen, onClose, btnRef, title, children }) => {
+  return (
+    <>
+      <SlideIn in={isOpen}>
+        {(styles) => (
+          <UIModal
+            finalFocusRef={btnRef}
+            onClose={onClose}
+            isOpen={true}
+            isCentered
+          >
+            <ModalOverlay opacity={styles.opacity} />
+            <ModalContent pb={3} {...styles}>
+              <ModalHeader>{title}</ModalHeader>
+              <ModalCloseButton />
+              <Divider />
+              <ModalBody>{children}</ModalBody>
+            </ModalContent>
+          </UIModal>
+        )}
+      </SlideIn>
+    </>
+  );
+};
