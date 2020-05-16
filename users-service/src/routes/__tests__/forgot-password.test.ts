55d683ea2858d16c40020a65ad2c9d15fead579f
Noble Obioma <nobioma1@gmail.com>
2020-05-16 22:06:57 +0100

    Todo tests

---
+++ users-service/src/routes/__tests__/forgot-password.test.ts
@@ -0,0 +1,5 @@
+describe('[POST /api/users/forgot-password] Forgot password', () => {
+  it.todo('returns an error 400 if field is not valid');
+
+  it.todo('return a status of 200 if field is valid');
+});
---
+++ users-service/src/routes/__tests__/reset-password.test.ts
@@ -0,0 +1,9 @@
+describe('[POST /api/users/reset-password] Reset user password', () => {
+  it.todo('returns an error 400 if fields are not valid');
+
+  it.todo('return a status of 200 if fields are valid');
+
+  it.todo('does not store plain password');
+
+  it.todo('reset user password and send response');
+});
