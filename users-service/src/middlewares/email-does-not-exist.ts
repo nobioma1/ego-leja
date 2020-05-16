d0f050720e02a5860c72e6f08544a5de3842a707
Noble Obioma <nobioma1@gmail.com>
2020-05-16 21:56:18 +0100

    check if email does not exist

---
+++ users-service/src/middlewares/email-does-not-exist.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+import { BadRequestError } from '@ego-leja/common';
+
+import { User } from '../models/user';
+
+export const emailDoesNotExist = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user = await User.findOne({
+    email: req.body.email,
+  });
+
+  if (user) {
+    throw new BadRequestError(`email ${user.email} is already in use`);
+  }
+
+  next();
+};
