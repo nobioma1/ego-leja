f7ff5bc5f1d48fc22e76f963df662d8f509ab669
Noble Obioma <nobioma1@gmail.com>
2020-05-17 13:02:44 +0100

    Add MONGO_DB_URI to config object

--- users-service/src/config/index.ts
+++ users-service/src/config/index.ts
@@ -3,5 +3,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 export const config = {
   JWT_KEY: process.env.JWT_KEY,
+  MONGO_DB_URI: process.env.MONGO_DB_URI,
   PORT: process.env.PORT || 5000,
 };
