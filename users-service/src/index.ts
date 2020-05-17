bb3f58d826b7033bdac51462c5a942af217f9867
Noble Obioma <nobioma1@gmail.com>
2020-05-17 13:01:40 +0100

    Add checks for env JWT_KEY and MONGO_DB_URI

--- users-service/src/index.ts
+++ users-service/src/index.ts
@@ -2,6 +2,14 @@ import { server } from './api/server';
 import { config } from './config';
 
 async function startServer() {
+  if (!config.JWT_KEY) {
+    throw new Error('JWT_KEY not provided');
+  }
+
+  if (!config.MONGO_DB_URI) {
+    throw new Error('MONGO_DB_URI not provided');
+  }
+
   server.listen(config.PORT, () => {
     console.log(`####🚀 Server listening on port: ${config.PORT} 🚀 ####`);
   });
