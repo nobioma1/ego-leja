0edbed6e37dc4706fb04360cf379995005430dd3
Noble Obioma <nobioma1@gmail.com>
2020-05-17 12:25:06 +0100

    Get all records with optional query

---
+++ records-service/src/routes/get-all.ts
@@ -0,0 +1,29 @@
+import { Router, Response, Request } from 'express';
+import { requireAuth } from '@ego-leja/common';
+
+import { Record } from '../models/record';
+import { TransactionType } from '../models/types/transaction-type';
+
+const router = Router();
+
+router.get('/api/records', requireAuth, async (req: Request, res: Response) => {
+  const params = req.query;
+
+  let queryParams = {};
+
+  if (params.trxType) {
+    queryParams = {
+      ...queryParams,
+      transactionType: params.trxType,
+    };
+  }
+
+  const records = await Record.find({
+    userId: req.currentUser.id,
+    ...queryParams,
+  });
+
+  res.status(200).send(records);
+});
+
+export { router as getRouter };
