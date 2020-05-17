07ff13594066637947587cde5310eadd5203ca90
Noble Obioma <nobioma1@gmail.com>
2020-05-17 10:48:57 +0100

    Middleware to check if record exists

---
+++ records-service/src/middlewares/record-exists.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { Record } from '../models/record';
+import { RecordDoc } from '../models/types/record-doc';
+import { NotFoundError } from '@ego-leja/common';
+
+declare global {
+  namespace Express {
+    interface Request {
+      record: RecordDoc;
+    }
+  }
+}
+export const recordExists = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const record = await Record.findById(req.params.recordId);
+
+  if (!record) {
+    throw new NotFoundError('Record Not Found');
+  }
+
+  req.record = record;
+  next();
+};
