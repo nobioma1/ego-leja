4162da7a625b1456012062a2acc98ccb7a9a5ef1
Noble Obioma <nobioma1@gmail.com>
2020-05-18 10:34:54 +0100

    Middleware to check if the record of recordId exists, throws a NotFoundError if it does not.

---
+++ transactions-service/src/middlewares/record-exists.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { Record, RecordDoc } from '../models/record';
+import { NotFoundError } from '@ego-leja/common';
+
+declare global {
+  namespace Express {
+    interface Request {
+      record: RecordDoc;
+    }
+  }
+}
+
+/**
+ * Checks if the record of recordId exists,
+ * throws a NotFoundError if it does not.
+ *
+ * Add record document to Request object if record exists
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+export const recordExists = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const record = await Record.findOne({
+    _id: req.params.recordId,
+    userId: req.currentUser.id,
+  });
+
+  if (!record) {
+    throw new NotFoundError('Record Not Found');
+  }
+
+  req.record = record;
+  next();
+};
