ec82facee29aac78168261e70825bfb7bd73177d
Noble Obioma <nobioma1@gmail.com>
2020-05-22 09:04:28 +0100

    Render user name from context

--- client/src/components/Sidebar/User.js
+++ client/src/components/Sidebar/User.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Flex, Box, Text } from '@chakra-ui/core';
 
+import { UserContext } from 'context/UserContext';
+
 export const User = () => {
+  const [{ user }] = useContext(UserContext);
+
   return (
     <Box ml={1}>
       <Flex justifyContent="center" direction="column" h="200px">
         <Text>Account Name</Text>
         <Text fontSize="xl" fontWeight="bold" opacity="0.9">
-          Mr. John Doe
+          {user.fullName}
         </Text>
       </Flex>
     </Box>
