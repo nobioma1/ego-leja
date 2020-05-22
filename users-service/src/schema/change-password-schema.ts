576a80828d293b9f2fe8ded69015d34fdc5138be
Noble Obioma <nobioma1@gmail.com>
2020-05-22 08:48:57 +0100

    Remove min character check

--- users-service/src/schema/change-password-schema.ts
+++ users-service/src/schema/change-password-schema.ts
@@ -1,7 +1,7 @@
 import Joi from '@hapi/joi';
 
 export const changePasswordSchema = Joi.object({
-  currentPassword: Joi.string().label('Current Password').min(8).required(),
+  currentPassword: Joi.string().label('Current Password').required(),
   newPassword: Joi.string().label('New Password').min(8).required(),
   confirmPassword: Joi.string().label('Confirm Password').min(8).required(),
 });
