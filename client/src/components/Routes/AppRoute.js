0f1afd568e5523e099afecfce0a6ff928b1f2700
Noble Obioma <nobioma1@gmail.com>
2020-05-22 09:02:43 +0100

    Add Protected route and Redirect

--- client/src/components/Routes/AppRoute.js
+++ client/src/components/Routes/AppRoute.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Router as BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  Router as BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
 import history from '../../history';
 import { Home } from 'pages/Home';
 import { Landing } from 'pages/Landing';
+import { ProtectedRoute } from './ProtectedRoute';
 
 export const AppRoute = () => {
   return (
     <BrowserRouter history={history}>
       <Switch>
         <Route path="/" component={Landing} exact />
-        <Route path="/home" component={Home} />
+        <ProtectedRoute path="/home" component={Home} />
+        <Redirect to="/" />
       </Switch>
     </BrowserRouter>
   );
