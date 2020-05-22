6e24f3fd164c59b22ffbc1ab14e17aa4456081bc
Noble Obioma <nobioma1@gmail.com>
2020-05-22 08:53:56 +0100

    Create user reducer

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -2,6 +2,28 @@ import React, { useReducer } from 'react';
 
 export const UserContext = React.createContext();
 
+export const userTypes = {
+  setUser: 'SET_USER',
+};
+
+const INITIAL_STATE = {
+  isLoading: false,
+  isLoggedIn: false,
+  user: null,
+};
+
+const UserReducer = (state, action) => {
+  switch (action.type) {
+    case userTypes.setUser:
+      return {
+        ...state,
+        user: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
 export const UserContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(UserReducer, INITIAL_STATE);
   return (
