885b69a13fcd01664a6708ed6a3f2f142aae038a
Noble Obioma <nobioma1@gmail.com>
2020-05-29 00:22:54 +0100

    Remove Ref

--- client/src/pages/Landing/TopBar.js
+++ client/src/pages/Landing/TopBar.js
@@ -3,26 +3,15 @@ import React from 'react';
 
 import logo from 'assets/logo.svg';
 
-export const TopBar = ({
-  onOpenLogin,
-  onOpenSignup,
-  loginBtnRef,
-  signUpBtnRef,
-}) => {
+export const TopBar = ({ onOpenLogin, onOpenSignup }) => {
   return (
     <Flex w="full" justifyContent="space-between">
       <Image src={logo} alt="Credit logo" h={12} />
       <Stack isInline>
-        <Button
-          ref={loginBtnRef}
-          onClick={onOpenLogin}
-          size="md"
-          variant="ghost"
-        >
+        <Button onClick={onOpenLogin} size="md" variant="ghost">
           Login
         </Button>
         <Button
-          ref={signUpBtnRef}
           onClick={onOpenSignup}
           size="md"
           variantColor="green"
