2b51d77df167923bfc30c48a241c31c823954aa2
Noble Obioma <nobioma1@gmail.com>
2020-05-29 00:12:43 +0100

    Wrap app with AuthContextProvider

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -4,16 +4,19 @@ import React from 'react';
 import { ThemeProvider } from 'components/Theme';
 import { AppRoute } from 'components/Routes';
 import { UserContextProvider } from 'context/UserContext';
+import { AuthContextProvider } from 'context/AuthContext';
 
 export const App = () => {
   return (
-    <ThemeProvider>
-      <CSSReset />
-      <Box>
-        <UserContextProvider>
-          <AppRoute />
-        </UserContextProvider>
-      </Box>
-    </ThemeProvider>
+    <AuthContextProvider>
+      <ThemeProvider>
+        <CSSReset />
+        <Box>
+          <UserContextProvider>
+            <AppRoute />
+          </UserContextProvider>
+        </Box>
+      </ThemeProvider>
+    </AuthContextProvider>
   );
 };
