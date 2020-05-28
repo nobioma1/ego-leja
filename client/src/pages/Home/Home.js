ceea77382c30262baaedfa46f1744bd27bfff079
Noble Obioma <nobioma1@gmail.com>
2020-05-29 00:24:32 +0100

    Add Loader to home if !user and wrap Home with AppContextProvider

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,34 +1,83 @@
-import React, { useState } from 'react';
-import { Box, Flex } from '@chakra-ui/core';
+import React, { useState, useContext, useEffect } from 'react';
+import { Box, Flex, Spinner, Image } from '@chakra-ui/core';
 
+import logo from 'assets/logo.svg';
 import { HomeRoute } from 'components/Routes';
 import { Sidebar } from 'components/Sidebar';
 import { TopBar } from 'components/TopBar';
+import { useRequest } from 'hooks/useRequest';
+import { UserContext } from 'context/UserContext';
+import { AppContextProvider } from 'context/AppContext';
+import { AddTransaction } from 'components/Transactions';
 
 export const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  return (
+  const {
+    state: { user },
+    setUser,
+  } = useContext(UserContext);
+
+  const { doRequest, errors } = useRequest({
+    method: 'get',
+    url: '/api/users',
+  });
+
+  useEffect(() => {
+    doRequest({
+      onSuccess: (user) => setUser(user),
+    });
+  }, []);
+
+  return !user ? (
+    <Flex
+      height="100vh"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <Image src={logo} alt="Credit logo" h={20} mr={2} />
+      {!errors ? (
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="green.200"
+          color="green.500"
+          size="xl"
+        />
+      ) : (
+        errors
+      )}
+    </Flex>
+  ) : (
     <Flex>
       <Box
         position={isOpen ? 'fixed' : 'static'}
         left="0"
         display={isOpen ? 'block' : ['none', 'none', 'block']}
         h={['100%', '100%', '100vh']}
-        w={['70%', '70%', '350px']}
+        w={['70%', '70%', '25%']}
         backgroundColor="white"
-        zIndex="popover"
+        zIndex="docked"
       >
         <Sidebar />
       </Box>
-      <Box h="100vh" w={['100%', '100%', '75%']} p={3}>
-        <Box px={[1, 1, 10]}>
+      <Flex
+        h="100vh"
+        w={['full', '70%', '75%']}
+        pt={[2, 2, 5]}
+        justifyContent="center"
+      >
+        <Flex w={['full', 'full', '4xl']} direction="column">
           <TopBar onClick={() => setIsOpen(!isOpen)} />
-        </Box>
-        <Box h={['92%', '92%', 'auto']} overflowY="auto">
-          <HomeRoute />
-        </Box>
-      </Box>
+          <Box mx={2}>
+            <AppContextProvider>
+              <HomeRoute />
+              <AddTransaction />
+            </AppContextProvider>
+          </Box>
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
