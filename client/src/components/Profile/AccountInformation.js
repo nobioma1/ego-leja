aa325b6190a9a5423d84e9c6654a83a2ada91541
Noble Obioma <nobioma1@gmail.com>
2020-05-29 00:20:24 +0100

    Remove Resend Verification message

--- client/src/components/Profile/AccountInformation.js
+++ client/src/components/Profile/AccountInformation.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Alert, AlertIcon, Text, Button, Box, Flex } from '@chakra-ui/core';
+import { Text, Button, Flex, Divider } from '@chakra-ui/core';
 
 import { AccountInformationForm } from './AccountInformationForm';
 import { AccordionLayout } from 'components/Shared';
@@ -9,54 +9,33 @@ export const AccountInformation = () => {
 
   return (
     <AccordionLayout title="Account Information" defaultIsOpen={true}>
-      <Alert status="warning">
-        <Flex
-          w="full"
-          justifyContent={['center', 'center', 'space-between']}
-          alignItems={['space-between', 'space-between', 'center']}
-          direction={['column', 'column', 'row']}
-        >
-          <Text>
-            <AlertIcon /> Account is not verified
-          </Text>
-          <Button
-            variant="outline"
-            variantColor="green"
-            size="sm"
-            mt={[2, 2, 0]}
-          >
-            Resend Verification Email
-          </Button>
-        </Flex>
-      </Alert>
-      <Box my={3}>
-        <Flex justifyContent="space-between" alignItems="center">
-          <Text textDecoration="underline" fontSize="lg">
-            User Details
-          </Text>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => setIsEdit(!isEdit)}
-          >
-            {isEdit ? 'Cancel' : 'Edit Details'}
-          </Button>
-        </Flex>
-        {isEdit ? (
-          <AccountInformationForm />
-        ) : (
-          <>
-            <Flex my={2} justifyContent="space-between">
-              <Text pr={2}>Country: </Text>
-              <Text>not set</Text>
-            </Flex>
-            <Flex my={2} justifyContent="space-between">
-              <Text pr={2}>State: </Text>
-              <Text>not set</Text>
-            </Flex>
-          </>
-        )}
-      </Box>
+      <Flex justifyContent="space-between" alignItems="center">
+        <Text fontSize="lg" fontWeight="bold">
+          Your Account Details
+        </Text>
+        <Button variant="outline" size="sm" onClick={() => setIsEdit(!isEdit)}>
+          {isEdit ? 'Cancel' : 'Edit Details'}
+        </Button>
+      </Flex>
+      {isEdit ? (
+        <AccountInformationForm />
+      ) : (
+        <>
+          <Flex my={3} justifyContent="space-between">
+            <Text pr={2} fontWeight="bold">
+              Country:{' '}
+            </Text>
+            <Text>not set</Text>
+          </Flex>
+          <Divider />
+          <Flex my={3} justifyContent="space-between">
+            <Text pr={2} fontWeight="bold">
+              State:{' '}
+            </Text>
+            <Text>not set</Text>
+          </Flex>
+        </>
+      )}
     </AccordionLayout>
   );
 };
