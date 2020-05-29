e29734f4ab63cbba28f88c1152433c7441607989
Noble Obioma <nobioma1@gmail.com>
2020-05-29 11:07:02 +0100

    Create AuthContextProvider to manage authentication

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import {
+  getSessionCookie,
+  clearSession,
+  setSessionCookie,
+} from 'utils/sessions';
+import { useRequest } from 'hooks/useRequest';
+
+export const AuthContext = React.createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [isAuthenticated, setAuthentication] = useState(getSessionCookie());
+  const history = useHistory();
+
+  const { doRequest } = useRequest({
+    method: 'post',
+    url: '/api/users/signout',
+  });
+
+  const setLogin = (user) => {
+    setSessionCookie(user.id);
+    setAuthentication(getSessionCookie());
+    history.push('/home');
+  };
+
+  const logout = () => {
+    doRequest({
+      onSuccess: () => {
+        clearSession();
+      },
+    });
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, logout, setLogin }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
