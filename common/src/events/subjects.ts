3446a637475ac5e95bb385abfb3f3877e3000601
Noble Obioma <nobioma1@gmail.com>
2020-06-07 20:44:09 +0100

    Organise, move logic into src folder

---
+++ common/src/events/subjects.ts
@@ -0,0 +1,4 @@
+export enum Subjects {
+  RECORD_CREATED = 'RECORD:Created',
+  RECORD_UPDATED = 'RECORD:Updated',
+}
--- common/src/index.ts
+++ common/src/index.ts
@@ -4,6 +4,12 @@ export * from './errors/not-authorized-error';
 export * from './errors/not-found-error';
 export * from './errors/request-validation-error';
 
+export * from './events/listener';
+export * from './events/publisher';
+export * from './events/records/record-created-event';
+export * from './events/records/record-updated-event';
+export * from './events/subjects';
+
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-fields';
