049da1af26f551148d6de03809f7117fd40189cc
Noble Obioma <nobioma1@gmail.com>
2020-06-08 13:25:02 +0100

    Render list of transactions

--- client/src/components/Transactions/TransactionTable.js
+++ client/src/components/Transactions/TransactionTable.js
@@ -2,30 +2,41 @@ import { Flex, Text, Box } from '@chakra-ui/core';
 import React from 'react';
 import moment from 'moment';
 
-export const TransactionTable = () => {
+export const TransactionTable = ({ transactions }) => {
+  const transactionRow = transactions.map((transaction, index) => (
+    <Flex my={2} key={transaction.id}>
+      <Text w="15%" textAlign="right">
+        {index + 1}
+      </Text>
+      <Text w="40%" textAlign="right">
+        {transaction.amount}
+      </Text>
+      <Text w="35%" textAlign="right">
+        {moment(transaction.createdAt).format('DD MMM, YYYY')}
+      </Text>
+    </Flex>
+  ));
+
   return (
     <Box minHeight={['150px', '150px', 'xs']}>
       <Flex borderBottomColor="Gray.500" borderBottomWidth="1px" pb={3}>
-        <Text w="35%" opacity="0.7">
-          Transaction Id
+        <Text w="15%" textAlign="right" opacity="0.7">
+          Serial
         </Text>
-        <Text w="30%" opacity="0.7">
+        <Text w="40%" textAlign="right" opacity="0.7">
           Amount
         </Text>
-        <Text w="30%" opacity="0.7">
+        <Text w="35%" textAlign="right" opacity="0.7">
           Date
         </Text>
       </Flex>
-      <Flex my={2}>
-        <Text w="35%">TRX5ecffa63</Text>
-        <Text w="30%">31,32424</Text>
-        <Text w="30%">{moment(new Date()).format('DD MMM, YYYY')}</Text>
-      </Flex>
-      <Flex my={2}>
-        <Text w="35%">TRX5ecffa63</Text>
-        <Text w="30%">31,32424</Text>
-        <Text w="30%">{moment(new Date()).format('DD MMM. YYYY')}</Text>
-      </Flex>
+      {transactions.length === 0 ? (
+        <Text textAlign="center" opacity="0.7" my={3}>
+          No transaction yet...
+        </Text>
+      ) : (
+        transactionRow
+      )}
     </Box>
   );
 };
