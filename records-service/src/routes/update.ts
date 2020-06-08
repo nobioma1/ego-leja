c82df750b4b40174d8c8652ccdd6e6c5b85c5fe3
Noble Obioma <nobioma1@gmail.com>
2020-06-08 13:18:23 +0100

    Publish event after update

--- records-service/src/routes/update.ts
+++ records-service/src/routes/update.ts
@@ -3,6 +3,8 @@ import { requireAuth, validateFields } from '@ego-leja/common';
 
 import { updateSchema } from '../schema/update-schema';
 import { recordExists } from '../middlewares/record-exists';
+import { RecordUpdatedPublisher } from '../events/publishers/record-updated-publisher';
+import { natsWrapper } from '../utils/nats-wrapper';
 
 const router = Router();
 
@@ -12,16 +14,28 @@ router.put(
   recordExists,
   validateFields(updateSchema),
   async (req: Request, res: Response) => {
-    const { name, description, isBadDebt } = req.body;
+    const { name, description, isBadDebt, dueDate } = req.body;
     const record = req.record;
 
     record.set({
       name,
       description,
       isBadDebt,
+      dueDate,
     });
 
     await record.save();
+
+    await new RecordUpdatedPublisher(natsWrapper.client).publish({
+      id: record._id,
+      amount: record.amount,
+      description: record.description,
+      dueDate: record.dueDate,
+      isBadDebt: record.isBadDebt,
+      name: record.name,
+      recordType: record.recordType,
+      userId: record.userId,
+    });
 
     res.status(200).send(record);
   }
