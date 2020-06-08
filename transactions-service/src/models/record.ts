30afd2b1cf4752e3e3c89adece78f790be7314ef
Noble Obioma <nobioma1@gmail.com>
2020-06-08 13:13:56 +0100

    Add properties to record - description, dueDate, isBadDebt, name, recordType

--- transactions-service/src/models/record.ts
+++ transactions-service/src/models/record.ts
@@ -1,14 +1,25 @@
 import mongoose from 'mongoose';
+import { RecordType } from '@ego-leja/common';
 
 interface RecordAttrs {
   id: string;
   amount: number;
+  description: string;
+  dueDate: Date;
+  isBadDebt: boolean;
+  name: string;
+  recordType: string;
   userId: string;
-  payable: number;
 }
 
 export interface RecordDoc extends mongoose.Document {
+  id: string;
   amount: number;
+  description: string;
+  dueDate: Date;
+  isBadDebt: boolean;
+  name: string;
+  recordType: RecordType;
   userId: string;
   payable: number;
 }
@@ -19,27 +30,55 @@ interface RecordModel extends mongoose.Model<RecordDoc> {
 
 const recordSchema = new mongoose.Schema(
   {
+    name: {
+      type: String,
+      required: true,
+    },
+    recordType: {
+      type: String,
+      enum: Object.values(RecordType),
+      required: true,
+    },
     amount: {
       type: Number,
       required: true,
       min: 0,
     },
+    payable: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    description: {
+      type: String,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+    isBadDebt: {
+      type: Boolean,
+      default: false,
+    },
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
     },
-    payable: {
-      type: Number,
-      required: true,
-    },
   },
   {
+    timestamps: true,
+    toObject: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.version;
+      },
+    },
     toJSON: {
       transform(doc, ret) {
         ret.id = ret._id;
         delete ret._id;
         delete ret.version;
-        delete ret.userId;
       },
     },
   }
@@ -48,7 +87,11 @@ const recordSchema = new mongoose.Schema(
 recordSchema.set('versionKey', 'version');
 
 recordSchema.statics.build = (attrs: RecordAttrs): RecordDoc => {
-  return new Record(attrs);
+  return new Record({
+    ...attrs,
+    _id: attrs.id,
+    payable: attrs.amount,
+  });
 };
 
 const Record = mongoose.model<RecordDoc, RecordModel>('Record', recordSchema);
--- transactions-service/src/test/helpers/create-record.ts
+++ transactions-service/src/test/helpers/create-record.ts
@@ -1,22 +1,33 @@
+import { RecordType } from '@ego-leja/common';
+
 import { generateID } from './generate-id';
 import { Record } from '../../models/record';
 
 export const createRecord = async ({
-  id = generateID(),
-  amount = 40000,
   userId,
+  id = generateID(),
+  name = 'Janes Test',
+  amount = 40000,
   payable = 40000,
+  dueDate = new Date(),
+  recordType = RecordType.LEND,
 }: {
   id?: string;
+  name?: string;
   amount?: number;
   userId: string;
   payable?: number;
+  dueDate?: Date;
+  recordType?: RecordType;
 }) => {
   const record = new Record({
     id,
+    name,
     amount,
     userId,
     payable,
+    dueDate,
+    recordType,
   });
   await record.save();
 
