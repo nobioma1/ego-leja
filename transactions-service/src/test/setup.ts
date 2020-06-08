84c960220dff3e91011b31563f0de6f9a4791b48
Noble Obioma <nobioma1@gmail.com>
2020-06-08 13:09:47 +0100

    Add nat mock in setup and clear mock before all

--- transactions-service/src/test/setup.ts
+++ transactions-service/src/test/setup.ts
@@ -14,11 +14,15 @@ declare global {
 
 let mongo: any;
 
+jest.mock('../utils/nats-wrapper');
+
 beforeAll(async () => {
+  jest.clearAllMocks();
+
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
-  process.env.JWT_KEY = 'someTestJETSecret';
+  process.env.JWT_KEY = 'someTestJWTSecret';
   process.env.MONGO_DB_URI = mongoUri;
 
   await mongoose.connect(process.env.MONGO_DB_URI!, {
