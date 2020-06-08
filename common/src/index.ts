c9033f03cffce8fc3c06361981e74ba35c69c0c5
Noble Obioma <nobioma1@gmail.com>
2020-06-08 13:12:26 +0100

    export record-deleted-event and record-type

--- common/src/index.ts
+++ common/src/index.ts
@@ -7,9 +7,12 @@ export * from './errors/request-validation-error';
 export * from './events/listener';
 export * from './events/publisher';
 export * from './events/records/record-created-event';
+export * from './events/records/record-deleted-event';
 export * from './events/records/record-updated-event';
 export * from './events/subjects';
 
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-fields';
+
+export * from './types/record-type';
