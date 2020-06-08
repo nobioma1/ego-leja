5158a0b6d4166ffc9b0d8364f2bf7caaba8710f6
Noble Obioma <nobioma1@gmail.com>
2020-06-08 13:23:33 +0100

    Request to offset amount

--- client/src/components/Transactions/OffsetForm.js
+++ client/src/components/Transactions/OffsetForm.js
@@ -6,6 +6,7 @@ import {
   InputLeftAddon,
   Text,
   Switch,
+  useToast,
 } from '@chakra-ui/core';
 import { Formik } from 'formik';
 import { FaCoins } from 'react-icons/fa';
@@ -13,13 +14,25 @@ import * as Yup from 'yup';
 
 import { AddOnInputField } from 'components/Shared';
 import symbols from 'utils/symbols';
+import { useRequest } from 'hooks/useRequest';
 
 const OffsetSchema = Yup.object().shape({
   amount: Yup.number().required('amount is required'),
 });
 
-export const OffsetForm = ({ amount, currency }) => {
+export const OffsetForm = ({
+  currency,
+  recordId,
+  payable,
+  onClose,
+  onSuccess,
+}) => {
+  const toast = useToast();
   const [offsetAmount, setOffsetAmount] = useState(0);
+  const { doRequest } = useRequest({
+    url: `/api/transactions/${recordId}`,
+    method: 'post',
+  });
 
   return (
     <Formik
@@ -28,8 +41,23 @@ export const OffsetForm = ({ amount, currency }) => {
         amount: offsetAmount,
       }}
       validationSchema={OffsetSchema}
-      onSubmit={(values, actions) => {
-        console.log('submitted');
+      onSubmit={async (values, { setSubmitting }) => {
+        setSubmitting(true);
+        await doRequest({
+          values,
+          onSuccess: (offset) => {
+            setSubmitting(false);
+            onClose();
+            toast({
+              title: 'Offset Successful',
+              description: `${offset.amount} has been deducted.`,
+              status: 'success',
+              duration: 3000,
+              position: 'top-right',
+            });
+            onSuccess();
+          },
+        });
       }}
     >
       {(props) => (
@@ -37,7 +65,7 @@ export const OffsetForm = ({ amount, currency }) => {
           <Flex justifyContent="space-between">
             <Text>Amount Left: </Text>
             <Text fontSize="xl">
-              {symbols[currency]} {amount}
+              {symbols[currency]} {payable}
             </Text>
           </Flex>
           <AddOnInputField
@@ -59,7 +87,7 @@ export const OffsetForm = ({ amount, currency }) => {
               size="md"
               onChange={() =>
                 offsetAmount === 0
-                  ? setOffsetAmount(amount)
+                  ? setOffsetAmount(payable)
                   : setOffsetAmount(0)
               }
             />
